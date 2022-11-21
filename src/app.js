ea78f28e434290fdfda8e59239dcc4673c5bc0df
ADOLFO BALSEIRO <abalseiro@hotmail.com>
2022-11-21 13:03:40 +0000

    Se crea la carta

--- src/app.js
+++ src/app.js
@@ -5,7 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  const symbol_up = ["♦", "♥", "♠", "♣"]
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K", "A"]
+  const symbol_down = ["♦", "♥", "♠", "♣"]
+  
+  let symb_up = symbol_up[Math.floor(Math.random() * symbol_up.length)]
+  let number = numbers[Math.floor(Math.random() * numbers.length)]
+  let symb_down = symbol_down[Math.floor(Math.random() * symbol_down.length)]
+  
+  const up = () =>{
+    document.getElementById('up')
+    if symb_up === 0 (
+      document.write('♦')
+      up.classList.add("color-red")
+    )
+  }
 };
