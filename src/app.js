085e3f31d88db7247c7b3a48c0cdb357a2fd9e72
ADOLFO BALSEIRO <abalseiro@hotmail.com>
2022-12-04 15:49:57 +0000

    Se modifica card

--- src/app.js
+++ src/app.js
@@ -6,19 +6,15 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = () => {
-  const symbol_up = ["♦", "♥", "♠", "♣"]
-  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K", "A"]
-  const symbol_down = ["♦", "♥", "♠", "♣"]
-  
-  let symb_up = symbol_up[Math.floor(Math.random() * symbol_up.length)]
-  let number = numbers[Math.floor(Math.random() * numbers.length)]
-  let symb_down = symbol_down[Math.floor(Math.random() * symbol_down.length)]
-  
-  const up = () =>{
-    document.getElementById('up')
-    if symb_up === 0 (
-      document.write('♦')
-      up.classList.add("color-red")
-    )
+  const number = ["A", 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K"];
+  let random_number = number[Math.floor(Math.random() * number.length)];
+  document.querySelector(".number").innerHTML = number[random_number];
+  const symbol = ["♦", "♥", "♠", "♣"];
+  let random_symbol = symbol[Math.floor(Math.random() * symbol.length)];
+  document.querySelector(".top-symbol").innerHTML = symbol[random_symbol];
+  document.querySelector(".symbol_down").innerHTML = symbol[random_symbol];
+  if (random_symbol <= 1) {
+    document.querySelector(".top-symbol").classList.add("color-red");
+    document.querySelector(".symbol_down").classList.add("color-red");
   }
 };
